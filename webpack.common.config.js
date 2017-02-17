32937f8b68a6f49dc6de77b6bfdb6cb8ee8f362d
Phil Lgr <pleger2@gmail.com>
2017-02-16 23:48:24 -0500

    Resolve .ts files for typescript support

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -5,6 +5,9 @@ function CommonConfig() {
             rules: [...require('./webpack-config/loaders.conf').common]
         },
         plugins: [...require('./webpack-config/plugins.conf').common],
+        resolve: {
+            extensions: ['.js', '.json', '.ts'],
+        },
     }
 }
 
